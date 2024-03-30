ad88a998d70c14db5c9a52d6f347f5c57cb51893
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-03-30 22:34:52 +0900

    [Setting]: 프로젝트 생성

---
+++ src/main/java/team/haedal/gifticionfunding/GifticionFundingApplication.java
@@ -0,0 +1,13 @@
+package team.haedal.gifticionfunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifticionFundingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifticionFundingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/haedal/gifticionfunding/GifticionFundingApplicationTests.java
@@ -0,0 +1,13 @@
+package team.haedal.gifticionfunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifticionFundingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
